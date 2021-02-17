fb859bc3b6ad2c7cc72b49fc7f865e3dde0d3b6e
Wei-Liang (Amos) Li <31527491+Rai-2018@users.noreply.github.com>
2021-02-17 14:58:06 -0800

    Finish imputing rest of the variable on training

--- src/q1_2.py
+++ src/q1_2.py
@@ -8,9 +8,10 @@ Created on Tue Feb 16 02:25:16 2021
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.stats import zscore
 from datetime import datetime
 from urllib.parse import urlparse
+from sklearn.impute import KNNImputer
+import reverse_geocoder as rg
 
 def process_age(x):
     if "-" in x:
@@ -40,18 +41,30 @@ def filter_age(cond):
     table = cases_train[cases_train['age'].str.contains(cond, regex=True)]
     return table
 
+def age_range(x):
+    if x>=60:
+        return 'senior'
+    elif 60 > x and x >= 18:
+        return 'adult'
+    elif 18 > x and x >= 5:
+        return 'child'
+    else:
+        return 'baby'
+
 def age_range_dummy(x):
     if "-" in x or "+" in x:
-        return True
+        return "True"
+    elif x:
+        return "False"
     else:
-        return False
+        return "Missing"
 
 def process_date_numerical(x):
     date_time_obj = datetime.strptime(x,'%d.%m.%Y')
     timestamp = datetime.timestamp(date_time_obj)
     return timestamp
 
-def process_date(x):
+def process_mode(x):
     if type(x) == np.ndarray:
         return x[0]
     else:
@@ -69,7 +82,39 @@ def parse_domain(x):
     # if '.gov' in domain:
     #     return "official"
     return domain
+
+
+def impute(criteria,target,dataset,func):
+    merge_criteria = criteria 
+    date_gb_mode = dataset.loc[dataset[target].notnull()]
+    date_gb_mode = date_gb_mode.groupby(merge_criteria)[target].agg(pd.Series.mode).to_frame()
+    date_gb_mode = date_gb_mode.rename(columns={target: 'fill'})
+    date_gb_mode.reset_index(inplace=True)
+    if func:
+        date_gb_mode['fill'] = date_gb_mode['fill'].apply(func)
     
+    dataset = dataset.merge(date_gb_mode,on = merge_criteria, how='left')
+    dataset[target] = dataset[target].fillna(dataset['fill'])
+    dataset.drop(columns=['fill'],inplace=True)
+    return dataset
+
+def process_province(dataset):
+    dataset = impute(['longitude','latitude'],'country',dataset,process_mode)
+    dataset.loc[(dataset.province == "taiwan"),'country'] = "China"
+    dataset = impute(['longitude','latitude'],'province',dataset,process_mode)
+
+    dataset['geom'] = list(zip(dataset['latitude'] , dataset['longitude']))
+    geo = rg.RGeocoder(mode=2, verbose=True)
+    
+    unique_arr = list(dataset["geom"])
+    unique_arr = pd.DataFrame(geo.query(unique_arr))
+    unique_arr['lon'] = unique_arr['lon'].astype(float)
+    unique_arr['lat'] = unique_arr['lat'].astype(float)
+    dataset = dataset.join(unique_arr[['admin1','admin2','cc','lon','lat']])
+    dataset.loc[(dataset.province.isnull()),'province'] = dataset['admin2']
+    dataset.drop(columns=['admin1','admin2','cc','lon','lat','geom'],inplace=True)
+    return dataset
+
 ##############################################################################
 # Loading the data and gathering frequency information
 cases_train = pd.read_csv('../data/cases_train.csv')
@@ -78,9 +123,86 @@ location = pd.read_csv('../data/location.csv')
 
 cases_train['latitude'] = cases_train['latitude'].astype(float)
 cases_train['longitude'] = cases_train['longitude'].astype(float)
+cases_train['province'] = cases_train['province'].str.lower()
+cases_train['country'] = cases_train['country'].str.lower()
+cases_train['additional_information'] = cases_train['additional_information'].str.lower()
+cases_train['source'] = cases_train['source'].str.lower()
+cases_train.dropna(subset=['sex', 'age','province'], how='all',inplace=True)
 
-# train = pd.read_csv('../data/cases_train.csv')
-# train = train.loc[train['longitude'] == 0]
+############################################################################################################
+# Impute age
+cases_train['age'] = cases_train['age'].fillna(value='')
+cases_train['age_range_ind'] = cases_train['age'].apply(age_range_dummy)
+cases_train['age'] = cases_train['age'].apply(process_age)
+cases_train['age'] = cases_train['age'].astype(float)
+cases_train['age'] = round(cases_train['age'],1)
+cases_train.loc[(cases_train.age < 1) & (cases_train.age >0),'age'] = 0
+
+
+cases_train = impute(['longitude','latitude','sex','date_confirmation'],'age',cases_train,process_mode)
+cases_train = impute(['longitude','latitude','date_confirmation'],'age',cases_train,process_mode)
+cases_train['age'] = cases_train['age'].fillna(cases_train['age'].mode().iloc[0])
+cases_train['age_range'] = cases_train['age'].apply(age_range)
+
+############################################################################################################
+# Imputation process
+# Imputating longitude / latitude variable 
+
+cases_train = cases_train.loc[cases_train.longitude.notnull()]
+###########################################################################################################
+
+# Imputating Additional information / Source 
+cases_train['additional_information'] = cases_train['additional_information'].fillna(value='')
+cases_train['source'] = cases_train['source'].fillna(value='')
+cases_train['source'] = cases_train['source'].apply(parse_domain)
+
+###########################################################################################################
+# # Imputating date confirmation
+cases_train = impute(['longitude','latitude'],'date_confirmation',cases_train,process_mode)
+cases_train = impute(['province','country'],'date_confirmation',cases_train,process_mode)
+
+# Fill in the remaining with mode - most frequent value
+cases_train['date_confirmation'] = cases_train['date_confirmation'].fillna(cases_train['date_confirmation'].mode().iloc[0])
+
+# Select the first one in date range
+cases_train['date_confirmation'] = cases_train['date_confirmation'].apply(process_date_range) 
+cases_train['date_confirmation'] = cases_train['date_confirmation'].apply(process_date_numerical) 
+############################################################################################################
+# Imputating provincial , country
+if __name__ == '__main__':
+    # missing_country = cases_train.loc[cases_train['country'].isnull()]
+    
+    cases_train = process_province(cases_train)
+    cases_train['province'] = cases_train['province'].replace('',np.nan)
+    cases_train = impute(['country'],'province',cases_train,process_mode)
+    cases_train.loc[(cases_train.province == 'CABA'),'province'] = 'Buenos Aires'
+    cases_train.loc[(cases_train.province.isnull()),'province'] = 'Invalid'
+
+############################################################################################################
+#Impute gender
+cases_train.loc[(cases_train.sex.isnull()),'sex'] = 'Not Available'
+
+###########################################################################################################
+    
+
+vc_imp = cases_train.notnull().sum().to_frame()
+vc_imp['prob'] = vc_imp/len(cases_train)
+vc_imp.reset_index(inplace=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+##############################################################################
+# Test Code
 
 # freq_age_init = cases_train['age'].value_counts().to_frame()
 # freq_age_init.reset_index(inplace=True)
@@ -100,120 +222,27 @@ cases_train['longitude'] = cases_train['longitude'].astype(float)
 # freq_lat_init = cases_train['latitude'].value_counts().to_frame()
 # freq_lat_init.reset_index(inplace=True)
 
-# unique_age = set(freq_age_init['index'])
+# freq_sex_init = cases_train['sex'].value_counts().to_frame()
+# freq_sex_init.reset_index(inplace=True)
 
 # vc_init = cases_train.notnull().sum().to_frame()
 # vc_init['prob'] = vc_init/len(cases_train)
 # vc_init.reset_index(inplace=True)
-
-############################################################################################################
-# Imputation process
-# Imputating longitude / latitude variable 
-  # Cannot impute because its all null for other attribute
-  # After removing the missing long, missing lat disappear
-# missing_long = cases_train.loc[cases_train.longitude.isnull()]
-cases_train = cases_train.loc[cases_train.longitude.notnull()]
-
-###########################################################################################################
-
-# Imputating Additional information / Source 
-cases_train['additional_information'] = cases_train['additional_information'].fillna(value='')
-cases_train['source'] = cases_train['source'].fillna(value='')
-
-cases_train['source'] = cases_train['source'].apply(parse_domain)
+    
+# freq_prov_imp = cases_train['province'].value_counts().to_frame()
+# freq_prov_imp.reset_index(inplace=True)
+    
 # freq_source_imp = cases_train['source'].value_counts().to_frame()
 # freq_source_imp.reset_index(inplace=True)
 
-############################################################################################################
-
-# Imputating age variable
-# Filling NaN with empty value and process age range and replace with the average
-# age_detail = pd.DataFrame(columns=['age','freq'])
-# age_regx = ['^0\.','90\+','80\+','-']
-# for regx in age_regx:
-#     age_detail = age_detail.append({'age':regx,'freq': len(filter_age(regx))}, ignore_index=True)
-# age_detail['prob'] = age_detail['freq'] / int(vc_init.loc[vc_init['index'] == 'age'][0])
-
-cases_train['age'] = cases_train['age'].fillna(value='')
-cases_train['age_range_ind'] = cases_train['age'].apply(age_range_dummy)
-cases_train['age'] = cases_train['age'].apply(process_age)
-cases_train['age'] = cases_train['age'].astype(float)
-cases_train['age'] = round(cases_train['age'],1)
-cases_train.loc[(cases_train.age < 1),'age'] = 0
-
-# freq_age_imp = cases_train['age'].value_counts().to_frame()
-# freq_age_imp.reset_index(inplace=True)
-
-############################################################################################################
-
-# Imputating date confirmation
 # missing_date = cases_train.loc[cases_train['date_confirmation'].isnull()]
 # date_gb_val = cases_train.groupby(['province','country'])['date_confirmation'].value_counts()
 # date_gb_agg = cases_train.groupby(['province','country'])['date_confirmation'].count()
 
-# Merge by 'longitude','latitude'
-merge_criteria = ['longitude','latitude']  # ['province','country']
-date_gb_mode = cases_train.loc[cases_train['date_confirmation'].notnull()]
-date_gb_mode = date_gb_mode.groupby(merge_criteria)['date_confirmation'].agg(pd.Series.mode).to_frame()
-date_gb_mode = date_gb_mode.rename(columns={'date_confirmation': 'fill'})
-date_gb_mode.reset_index(inplace=True)
-date_gb_mode['fill'] = date_gb_mode['fill'].apply(process_date)
-
-cases_train = cases_train.merge(date_gb_mode,on = merge_criteria, how='left')
-cases_train['date_confirmation'] = cases_train['date_confirmation'].fillna(cases_train['fill'])
-cases_train.drop(columns=['fill'],inplace=True)
-
-
-# Merge by province , country
-merge_criteria = ['province','country']  # ['longitude','latitude']
-date_gb_mode = cases_train.loc[cases_train['date_confirmation'].notnull()]
-date_gb_mode = date_gb_mode.groupby(merge_criteria)['date_confirmation'].agg(pd.Series.mode).to_frame()
-date_gb_mode = date_gb_mode.rename(columns={'date_confirmation': 'fill'})
-date_gb_mode.reset_index(inplace=True)
-date_gb_mode['fill'] = date_gb_mode['fill'].apply(process_date)
-
-cases_train = cases_train.merge(date_gb_mode,on = merge_criteria, how='left')
-cases_train['date_confirmation'] = cases_train['date_confirmation'].fillna(cases_train['fill'])
-cases_train.drop(columns=['fill'],inplace=True)
-
-
-# Fill in the remaining with mode - most frequent value
-cases_train['date_confirmation'] = cases_train['date_confirmation'].fillna(cases_train['date_confirmation'].mode().iloc[0])
-
-# Select the first one in date range
-cases_train['date_confirmation'] = cases_train['date_confirmation'].apply(process_date_range) 
-cases_train['date_confirmation'] = cases_train['date_confirmation'].apply(process_date_numerical) 
-
 # missing_date = cases_train.loc[cases_train['date_confirmation'].isnull()]
-freq_date_imp = cases_train['date_confirmation'].value_counts().to_frame()
-freq_date_imp.reset_index(inplace=True)
-
-
-
-vc_imp = cases_train.notnull().sum().to_frame()
-vc_imp['prob'] = vc_imp/len(cases_train)
-vc_imp.reset_index(inplace=True)
-
-############################################################################################################
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# freq_date_imp = cases_train['date_confirmation'].value_counts().to_frame()
+# freq_date_imp.reset_index(inplace=True)
+##############################################################################
 
 
 
