9a79b95b9dfc2f9d0ef4d41d8330305cfda320b4
Bianca <Bianca6298@ymail.com>
2021-02-19 14:31:52 -0800

    cleaned up data in locations
    Removed any negative valued case counts. We only have negative 'Active case' values, but I removed those rows.
    There were 6 of them.

--- Trans.py
+++ Trans.py
@@ -3,17 +3,20 @@ import pandas as pd
 
 location_dat = pd.read_csv('location.csv')
 bool_find = location_dat['Country_Region'].str.contains('US')
+#quick cleaning, as I've noticed negative values in my active cases, which makes no sense. (you can't have negative covid)
+bad_data = location_dat[["Confirmed","Deaths","Recovered","Active"]]
+#bad_count = bad_data[bad_data < 0].count()
+#print(bad_count)
+location_dat = location_dat[(location_dat['Active'] >= 0) | (location_dat['Active'].isnull())]
 
 #isolate US data
 US_loc = location_dat.loc[bool_find]
 US_inv = location_dat.loc[~bool_find]
-#US_loc.to_csv(r'US_loc.csv',index=False)
-#US_inv.to_csv(r'US_inv.csv',index=False)
 
 #transformation on US_loc
 #split US_loc by state
 states = US_loc['Province_State'].unique().tolist()
-states.remove("Recovered")
+#states.remove("Recovered")
 #print(states)
 states_df = dict(tuple(US_loc.groupby('Province_State')))
 
@@ -41,4 +44,4 @@ for loc in states:
 	new_line = pd.DataFrame([[loc,"US",date,lat,long_,confirmed,deaths,recovered,active,combined_key,inc,case_fat]], columns = ["Province_State","Country_Region","Last_Update","Lat","Long_","Confirmed","Deaths","Recovered","Active","Combined_Key","Incidence_Rate","Case-Fatality_Ratio"])
 	US_inv = pd.concat([US_inv, new_line], axis=0)
 
-US_inv.to_csv(r'Location_T.csv',index=False)
+US_inv.to_csv(r'location_transformed.csv',index=False)
