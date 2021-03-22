3a44825120250f95f77041f0df9ba9f98bbc6e81
Wei-Liang (Amos) Li <31527491+Rai-2018@users.noreply.github.com>
2021-03-21 17:09:30 -0700

    Reupdate catboost code
    Change some parameters, added more metrices for overfitting

--- src/catboost_model.py
+++ src/catboost_model.py
@@ -19,21 +19,16 @@ import matplotlib.pyplot as plt
 
 train = pd.read_csv(r'../results/cases_train_preprocessed.csv',dtype=object)
 # There are no information that can be introduce from this column, drop it
-train.drop(columns=['land','Last_Update'],inplace=True)
+train.drop(columns=['land','Last_Update','province','country'],inplace=True)
 # Reloading into dataframe cause empty strings to be NaN again
 train['source'] = train['source'].fillna(value='')
-
-test =  pd.read_csv(r'../results/cases_test_preprocessed.csv',dtype=object)
-# There are no information that can be introduce from this column, drop it
-test.drop(columns=['Last_Update'],inplace=True)
-# Reloading into dataframe cause empty strings to be NaN again
-test['source'] = test['source'].fillna(value='')
 
 
 #%% 
 
-cat_feature=['sex','province','country',
-             'source','age_range_ind','age_range']
+# cat_feature=['sex','province','country',
+#              'source','age_range_ind','age_range']
+cat_feature=['sex','source','age_range_ind','age_range']
 text_feature = ['additional_information']
 
 df_tr, df_va = train_test_split(train, test_size=0.2, random_state = 1)
@@ -58,16 +53,17 @@ df_va_data = Pool(data = df_va_x,
 model = CatBoostClassifier(n_estimators=300, 
                             task_type="GPU",
                             devices='0:1',
-                            learning_rate=1,
-                            depth=10,
+                            learning_rate=0.5,
+                            depth=14,
                             loss_function='MultiClass',
                             random_seed = 1,
-                            l2_leaf_reg = 2,
+                            l2_leaf_reg = 0.2,
                             auto_class_weights = 'Balanced')
 
 model.fit(df_tr_data)
 filename = 'catboost_classifier.pkl'
 pickle.dump(model, open(filename, 'wb'))
+
 #%% Evaluation
 label = ['hospitalized', 'nonhospitalized','recovered', 'deceased']
 
@@ -78,17 +74,17 @@ ev_tr_matrix = confusion_matrix(df_tr_y, df_tr_pred,labels=label)
 ConfusionMatrixDisplay(ev_tr_matrix).plot()
 
 
+
 df_va_pred = model.predict(df_va_data)
 ev_va_report = classification_report(df_va_y, df_va_pred)
 ev_va_accuracy = accuracy_score(df_va_y, df_va_pred)
 ev_va_matrix = confusion_matrix(df_va_y, df_va_pred,labels=label)
 ConfusionMatrixDisplay(ev_va_matrix).plot()
 
-
 #%% 
 
 n_estimators = [10, 25, 50, 100, 200, 300, 400]
-# n_estimators = [10]
+# n_estimators = [100]
 plt_tr_precision = []
 plt_tr_recall = []
 plt_tr_f1 = []
@@ -113,15 +109,15 @@ for n in n_estimators:
     tmp_model.fit(df_tr_data)
 
     tmp_tr_pred = tmp_model.predict(df_tr_data)
-    tmp_tr_precision = precision_score(df_tr_y, tmp_tr_pred, average='weighted')
-    tmp_tr_recall = recall_score(df_tr_y, tmp_tr_pred, average='weighted')
-    tmp_tr_f1 = f1_score(df_tr_y, tmp_tr_pred, average='weighted')
+    tmp_tr_precision = precision_score(df_tr_y, tmp_tr_pred, average='macro')
+    tmp_tr_recall = recall_score(df_tr_y, tmp_tr_pred, average='macro')
+    tmp_tr_f1 = f1_score(df_tr_y, tmp_tr_pred, average='macro')
     tmp_tr_accuracy = accuracy_score(df_tr_y, tmp_tr_pred)
     
     tmp_va_pred = tmp_model.predict(df_va_data)
-    tmp_va_precision = precision_score(df_va_y, tmp_va_pred, average='weighted')
-    tmp_va_recall = recall_score(df_va_y, tmp_va_pred, average='weighted')
-    tmp_va_f1 = f1_score(df_va_y, tmp_va_pred, average='weighted')
+    tmp_va_precision = precision_score(df_va_y, tmp_va_pred, average='macro')
+    tmp_va_recall = recall_score(df_va_y, tmp_va_pred, average='macro')
+    tmp_va_f1 = f1_score(df_va_y, tmp_va_pred, average='macro')
     tmp_va_accuracy = accuracy_score(df_va_y, tmp_va_pred)
     
     plt_tr_precision.append(tmp_tr_precision)
@@ -144,18 +140,25 @@ plt.plot(n_estimators, plt_tr_precision,label="training")
 plt.plot(n_estimators, plt_va_precision,label="validation")
 plt.legend()
 plt.title("Plot of weighted precision against n_est")
-plt.yticks(np.arange(0.6, 0.9, 0.05))
+# plt.yticks(np.arange(0.7, 0.9, 0.02))
 plt.show()
 
-plt.figure(2)
-plt.plot(n_estimators, plt_tr_acc,label="training")
-plt.plot(n_estimators, plt_va_acc,label="validation")
+
+plt.figure(3)
+plt.plot(n_estimators, plt_tr_f1,label="training")
+plt.plot(n_estimators, plt_va_f1,label="validation")
 plt.legend()
-plt.title("Plot of accuracy against n_est")
-plt.yticks(np.arange(0.6, 0.9, 0.05))
+plt.title("Plot of weighted f1 against n_est")
+# plt.yticks(np.arange(0.7, 0.9, 0.02))
 plt.show()
 
-
+plt.figure(4)
+plt.plot(n_estimators, plt_tr_recall,label="training")
+plt.plot(n_estimators, plt_va_recall,label="validation")
+plt.legend()
+plt.title("Plot of weighted recall against n_est")
+# plt.yticks(np.arange(0.7, 0.9, 0.02))
+plt.show()
 
 del tmp_model
 del tmp_tr_pred
@@ -184,3 +187,19 @@ del tmp_va_accuracy
 # ev_va_precision = precision_score(df_va_y, df_va_pred, average=None)
 # ev_va_recall = recall_score(df_va_y, df_va_pred, average=None)
 # ev_va_f1 = f1_score(df_va_y, df_va_pred, average=None)
+
+# test =  pd.read_csv(r'../results/cases_test_preprocessed.csv',dtype=object)
+# # There are no information that can be introduce from this column, drop it
+# test.drop(columns=['Last_Update'],inplace=True)
+# # Reloading into dataframe cause empty strings to be NaN again
+# test['source'] = test['source'].fillna(value='')
+
+# a1 = precision_score(df_va_y, df_va_pred, average="macro")
+# a2 = precision_score(df_va_y, df_va_pred, average="micro")
+# a3 = precision_score(df_va_y, df_va_pred, average="weighted")
+
+# a4 = recall_score(df_va_y, df_va_pred, average="macro")
+# a5 = recall_score(df_va_y, df_va_pred, average="micro")
+# a6 = recall_score(df_va_y, df_va_pred, average="weighted")
+
+
