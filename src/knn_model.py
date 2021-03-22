bfe6c21e9e6f1884d40b21144a45fe705231a733
BiancaBlackwell <bianca6298@ymail.com>
2021-03-21 17:30:05 -0700

    2.1,2.2 in _model, 2.3,2.4 in model_evaluation

--- src/knn_model.py
+++ src/knn_model.py
@@ -10,36 +10,12 @@ import matplotlib.pyplot as plt
 import pickle
 
 #Function for Hot-Encoding a DF, returns the DF with no categorical data
-def Hot_Encode(data,data2):
+def Hot_Encode(data):
 	temp = pd.get_dummies(data.sex, prefix='sex')
 	data = data.join(temp)
 	data = data.drop(columns = "sex")
-	#Notice we need our country and province encoding to include every possible value our model should handle
-	#I.e, both training and test data in our case
-	temp = data['country']
-	temp2 = data2['country']
-
-	temp = pd.concat([temp,temp2],ignore_index = True)
-	temp = pd.get_dummies(temp, prefix = 'country')
-	#only take the entries that correspond to our data at hand
-	temp = temp.head(len(data.index))
-	data = data.join(temp)
 	data = data.drop(columns = "country")
-
-	#Do the same thing for province
-	temp = data['province']
-	temp2 = data['province']
-
-	temp = pd.concat([temp,temp2], ignore_index = True)
-	temp = pd.get_dummies(temp, prefix = 'province')
-	temp = temp.head(len(data.index))
-	data = data.join(temp)
 	data = data.drop(columns = "province")
-
-	#Back to hot encoding the rest of the data..
-	# temp = pd.get_dummies(data.outcome, prefix = 'outcome')
-	# data = data.join(temp)
-	# data = data.drop(columns = "outcome")
 	temp = pd.get_dummies(data.age_range, prefix = 'age_range')
 	data = data.join(temp)
 	data = data.drop(columns = "age_range")
@@ -47,36 +23,24 @@ def Hot_Encode(data,data2):
 	#The returned data is entirely hot-encoded
 	return data
 
-
 #load in the data
 train = pd.read_csv(r'../results/cases_train_preprocessed.csv',dtype=object)
-test =  pd.read_csv(r'../results/cases_test_preprocessed.csv',dtype=object)
+#test =  pd.read_csv(r'../results/cases_test_preprocessed.csv',dtype=object)
 
 #Dropping land column, age_range_ind, and Last_Updated, and source
 train = train.drop(columns = "land")
 train = train.drop(columns = "Last_Update")
-test = test.drop(columns = "Last_Update")
 train = train.drop(columns = "age_range_ind")
-test = test.drop(columns = "age_range_ind")
 train =train.drop(columns = "source")
-test = test.drop(columns = "source")
-
 
 #Hot-encoding (both to test and train)
 train_copy = train.copy()
-test_copy = test.copy()
-train = Hot_Encode(train,test_copy)
-test = Hot_Encode(test,train_copy)
+train = Hot_Encode(train)
 
 #Handling additional_information (0 for no data, 1 for any data)
 train.loc[train['additional_information'] == '[]', 'add_info'] = 0
 train.loc[train['additional_information'] != '[]', 'add_info'] = 1
 train = train.drop(columns = "additional_information")
-
-#Spliting Dataset
-
-# MY LAPTOP SUCKS THIS IS NEEDED SO IT RUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUNS DELETE BEFORE ACTUALLY RUNNING IT
-train = train.head(10000)
 
 #Split training data into data and labels
 x = train.drop(columns = 'outcome')
@@ -95,11 +59,14 @@ k_neigh = 5
 classifier = KNeighborsClassifier(n_neighbors = k_neigh)
 classifier.fit(x_tr, y_tr)
 
-knnPickle = open('knn_classifier.pkl','wb')
+knnPickle = open('../models/knn_classifier.pkl','wb')
 pickle.dump(classifier, knnPickle)
 
-#Save my x_te, y_te data to open in new file
+#Save my validation data x_te, y_te data to open in new file
 y_te.to_csv('y_te.csv', index=False)
 pd.DataFrame(x_te).to_csv('x_te.csv', index =False)
+#saving my training data since apperently i have to 'load my model' later on...
+y_tr.to_csv('y_tr.csv', index= False)
+pd.DataFrame(x_tr).to_csv('x_tr.csv', index=False)
 
-print("Finished Constructing KNN model with k neightbours = " + str(k_neigh))
+print("Finished Constructing KNN model with k neightbours = " + str(k_neigh) + ".")
--- src/knn_model_evaluation.py
+++ src/knn_model_evaluation.py
@@ -3,14 +3,35 @@
 import pandas as pd
 import numpy as np
 from sklearn.metrics import classification_report, confusion_matrix
+from mlxtend.plotting import plot_decision_regions
 import matplotlib.pyplot as plt
 import pickle
 
-knn = pickle.load(open('knn_classifier.pkl', 'rb'))
+#2.3 "load the saved models from task 2.2, ..."
+#assuming i'm not allowed to keep working in the same file then....
+#I only see us uploading .main though, so this can be streamlined.
+classifier = pickle.load(open('knn_classifier.pkl', 'rb'))
 y_te = pd.read_csv('y_te.csv',dtype = object)
 x_te = pd.read_csv('x_te.csv')
-print(y_te)
-print(x_te)
-prediction = knn.predict(x_te)
+y_tr = pd.read_csv('y_tr.csv',dtype = object)
+x_tr = pd.read_csv('x_tr.csv')
+
+print("Predicting on validation data with knn model, hang on, this might take a minute.")
+prediction = classifier.predict(x_te)
+print("Finished Prediction, calculating metrics.")
 print(confusion_matrix(y_te,prediction))
 print(classification_report(y_te,prediction))
+
+
+print("Predicting on training data with knn model, hang on, this might take a minute.")
+prediction = classifier.predict(x_tr)
+print("Finished Prediction, calculating metrics.")
+print(confusion_matrix(y_tr,prediction))
+print(classification_report(y_tr,prediction))
+
+#Graphs -> decided metrics were good, no graphs needed
+# plot_decision_regions(x_te,y_te, clf=clf, legend = 2)
+# plt.xlabel('X')
+# plt.ylabel('Y')
+# plt.title('Knn with k = 5')
+# plt.show()
