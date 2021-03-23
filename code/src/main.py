25b301f135c0905818ce5964a83e636d2f5cc4a5
Wei-Liang (Amos) Li <31527491+Rai-2018@users.noreply.github.com>
2021-03-22 22:41:38 -0700

    Final commit to milestone 2

--- code/src/main.py
+++ code/src/main.py
@@ -5,10 +5,10 @@ Created on Thu Feb 18 17:17:51 2021
 @author: wlian
 """
 
-from q1_2 import q1_2
-from q1_3 import q1_3
-from q1_4 import q1_4
-from q1_5 import q1_5
+# from q1_2 import q1_2
+# from q1_3 import q1_3
+# from q1_4 import q1_4
+# from q1_5 import q1_5
 import os 
 
 import pandas as pd
